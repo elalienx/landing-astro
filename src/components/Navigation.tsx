5cc2c319ee70cbc0e189fa4bfef22854caaee1a4
Eduardo Alvarez <eduardo.alvarez.nowak@schibsted.com>
2025-10-15 15:33:04 +0200

    adding design

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,23 @@
+import styles from "../styles/navigation.module.css";
+
+export default function Navigation() {
+  return (
+    <nav className={styles.navigation}>
+      <span className="logo">Lendo</span>
+      <div className={styles.links}>
+        <a href="/" className={styles.link}>
+          Privat
+        </a>
+        <a href="/business" className={styles.link}>
+          Foregslatan
+        </a>
+        <a href="/mortgage" className={styles.link}>
+          Bolan
+        </a>
+      </div>
+      <a href="https://inbox.lendo.se" className={styles.link}>
+        Logga in 🔐
+      </a>
+    </nav>
+  );
+}
