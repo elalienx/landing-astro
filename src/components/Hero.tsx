b32959978481b1dafaea82ab313af93c4f5183ea
Eduardo Alvarez <eduardo.alvarez.nowak@schibsted.com>
2025-10-15 15:22:54 +0200

    adding pages

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,15 @@
 import styles from "../styles/hero.module.css";
 
-export default function Hero() {
+interface Props {
+  product: string;
+}
+
+export default function Hero({ product }: Props) {
   return (
     <header className={styles.hero}>
       <div className="content">
         <h1>Hackathon 2025 🎃</h1>
+        <h2>{product}</h2>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus qui
           totam, deserunt velit omnis ratione sequi accusantium rem adipisci est
