c0343b8b613051537c9ac235576b894bc4057f15
Eduardo Alvarez <eduardo.alvarez.nowak@schibsted.com>
2025-10-15 11:02:39 +0200

    adding component

---
+++ src/components/IncrementalCounter.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+interface Props {
+  /** The title to show on the component. */
+  label: string;
+}
+
+export default function IncrementalCounter({ label }: Props) {
+  // Local state
+  const [value, setValue] = useState(0);
+
+  return (
+    <div>
+      {/* Title */}
+      <span>{label}</span>
+
+      {/* Controls */}
+      <div className="controls">
+        <button onClick={() => setValue(value - 1)} disabled={value <= 0}>
+          -
+        </button>
+        <input
+          type="number"
+          pattern="\d*"
+          value={value}
+          onChange={(event) => setValue(Number(event.target.value))}
+        />
+        <button onClick={() => setValue(value + 1)} disabled={value >= 10}>
+          +
+        </button>
+      </div>
+    </div>
+  );
+}
