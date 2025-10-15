bf63c640c1aa9cee05abecfe1610e5a78df308ef
Eduardo Alvarez <eduardo.alvarez.nowak@schibsted.com>
2025-10-15 13:22:35 +0200

    adding code

--- README.md
+++ README.md
@@ -32,14 +32,23 @@ To learn more about the folder structure of an Astro project, refer to [our guid
 
 All commands are run from the root of the project, from a terminal:
 
-| Command                   | Action                                           |
-| :------------------------ | :----------------------------------------------- |
-| `pnpm install`             | Installs dependencies                            |
+| Command                | Action                                           |
+| :--------------------- | :----------------------------------------------- |
+| `pnpm install`         | Installs dependencies                            |
 | `pnpm dev`             | Starts local dev server at `localhost:4321`      |
 | `pnpm build`           | Build your production site to `./dist/`          |
 | `pnpm preview`         | Preview your build locally, before deploying     |
 | `pnpm astro ...`       | Run CLI commands like `astro add`, `astro check` |
 | `pnpm astro -- --help` | Get help using the Astro CLI                     |
+
+## 🧑‍💻 Client directives
+
+These are the flags you pass to the main page to decide when to make your React components interactive:
+
+1. `client:load`: Hydrates the component as soon as the page loads.
+1. `client:visible`: Hydrates the component when it becomes visible in the viewport.
+1. `client:idle`: Hydrates the component when the browser is idle.
+1. `client:media="(query)"`: Hydrates only when a specific media query matches.
 
 ## 👀 Want to learn more?
 
---
+++ src/components/Kreddy.tsx
@@ -0,0 +1,57 @@
+export default function Kreddy() {
+  return (
+    <>
+      {/* Toggle between them using CSS */}
+      <Mobile />
+      <Desktop />
+    </>
+  );
+}
+
+function Mobile() {
+  return (
+    <section
+      className="kreddy-mobile"
+      style={{ backgroundColor: "pink", color: "maroon" }}
+    >
+      <img src="./app-banner.webp" />
+      <h2>Din privatekonomi - direkt i mobilen med Kreddy</h2>
+      <p>
+        I Lendos app, Kreddy fär du tillgàng till ditt kreditbetyg som vi
+        inhämtar frän UC, utan att en kreditupplysning tas pà dig. Utöver att se
+        ditt kreditbetyg fär du även en översikt av dina befintliga làn och
+        krediter och vill du ha hjälp att samla dem via Lendo sà hjälper vi dig
+        med din ansökan. Allt і Kreddy-appen är gratis.
+      </p>
+      <h3>Ladda ner nu</h3>
+      <img src="./app-store-badge.svg" loading="lazy" />
+      <img src="./google-play-badge.svg" loading="lazy" />
+    </section>
+  );
+}
+
+function Desktop() {
+  return (
+    <section
+      className="kreddy-desktop"
+      style={{ backgroundColor: "aliceblue", color: "navy" }}
+    >
+      <div className="content">
+        <h2>Din privatekonomi - direkt i mobilen med Kreddy</h2>
+        <p>
+          I Lendos app, Kreddy fär du tillgäng till ditt kreditbetyg som vi
+          inhämtar fràn UC, utan att en kreditupplysning tas pà dig. Utöver att
+          se ditt kreditbetyg fär du även en översikt av dina befintliga län och
+          krediter och vill du ha hjälp att samla dem via Lendo sà hjälper vi
+          dig med din ansökan. Allt i Kreddy-appen är gratis.
+        </p>
+        <div>
+          <p>Scanna QR-koden med mobilkameran för att ladda ner appen</p>
+          <img src="./arrow.svg" loading="lazy" />
+          <img src="./qrcode-lendose.svg" loading="lazy" />
+        </div>
+      </div>
+      <img src="./app-banner.webp" loading="lazy" />
+    </section>
+  );
+}
